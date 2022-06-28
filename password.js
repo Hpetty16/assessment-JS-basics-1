e96a0a402edb6b992ee5fd7041af80e630c138da
Hpetty16 <petty.haywood@gmail.com>
2022-06-28 10:43:17 -0400

    this is the updated version with the correct answers

--- password.js
+++ password.js
@@ -1,3 +1,4 @@
+
 let password = (`Bull19641997`)
 
 if(password.length>=10){
@@ -26,83 +27,54 @@ if(password.length>=10){
  //(/[^A-Za-z0-9]/.test(password)= false){
   console.log(`Password is a failure`)
  }
-
-//I've tried everything and the only thing i could get to work was the password length lol. Hopefully I can get some extra credit for heading in  the right direction lol  =)
  
+ //I've tried everything and the only thing i could get to work was the password length lol. Hopefully I can get some extra credit for heading in  the right direction lol  =)
 
 
 
-// let password = (`Bull19971964`)
-// function validate(password) {
-//   var minMaxLength = /^[\s\S]{8,32}$/,
-//       upper = /[A-Z]/,
-//       lower = /[a-z]/,
-//       number = /[0-9]/,
-//       special = /[^A-Za-z0-9]/,
-//       count = 0;
 
-//   if (minMaxLength.test(password)) {
-//       // Only need 3 out of 4 of these to match
-//       if (upper.test(password)) count++;
-//       if (lower.test(password)) count++;
-//       if (number.test(password)) count++;
-//       if (special.test(password)) count++;
-//   }
-//   if(password=== count){
-//     console.log(`Password is all set`)
-//   }else{
-//     console.log(`You need to fix ya password ASAP`)
-//   }
-//   return count >= 3;
+
+
+
+
+
+ //this is the longer way to do and solve the problem
+
+// let password = 'sdkdjd'
+
+// //split string to array so we can loop over it
+// let passwordSplit = password.split('')
+
+// // console.log(passwordSplit)
+
+// let alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','x','y','z']
+
+// let nums = [0,1,2,3,4,5,6,7,8,9]
+
+// if(password.length >= 10){
+//     for(let i = 0;i < passwordSplit.length;i++){
+//         if(alphabet.includes(passwordSplit[i])){
+//             for(let j= i+1;j < passwordSplit.length;j++){
+//                 if(nums.includes(+passwordSplit[j])){
+//                     return console.log(`password meets requirements`)
+//                 }
+//             }
+//         } return console.log(`password doesnt meet requiremenets`)
+//     }
+// } else {
+//     console.log(`did not meet length requirements,try again with 10 digits`)
 // }
-  
-// let password = (`Bull196411`)
 
-// if(password.length>=10){
-//   console.log(`Password is a success`)
-// } else{
-//   console.log(`Password is a failure`)
+
+// this is the easy short way to do this 
+
+
+// if (password.length >= 10) {
+//   if (/\d/.test(password) && /\D/.test(password)) {
+//     console.log(`success`);
+//   } else {
+//     console.log(`try again`);
+//   }
+// } else {
+//     console.log(`password does not meet length requirements`)
 // }
-
-// function checkPasswordValidation(value) {
-//   const isWhitespace = /^(?=.*\s)/;
-//   if (isWhitespace.test(value)) {
-//     return "Password must not contain Whitespaces.";
-//   }
-
-
-//   const isContainsUppercase = /^(?=.*[A-Z])/;
-//   if (!isContainsUppercase.test(value)) {
-//     return "Password must have at least one Uppercase Character.";
-//   }
-
-
-//   const isContainsLowercase = /^(?=.*[a-z])/;
-//   if (!isContainsLowercase.test(value)) {
-//     return "Password must have at least one Lowercase Character.";
-//   }
-
-
-//   const isContainsNumber = /^(?=.*[0-9])/;
-//   if (!isContainsNumber.test(value)) {
-//     return "Password must contain at least one Digit.";
-//   }
-
-
-//   const isContainsSymbol =
-//     /^(?=.*[~`!@#$%^&*()--+={}\[\]|\\:;"'<>,.?/_₹])/;
-//   if (!isContainsSymbol.test(value)) {
-//     return "Password must contain at least one Special Symbol.";
-//   }
-
-
-//   const isValidLength = /^.{10,16}$/;
-//   if (!isValidLength.test(value)) {
-//     return "Password must be 10-16 Characters Long.";
-//   }
-// }
-// console.log(password)
-
-
-// this the regex func. 
-//     /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[#$@!%&*?]) A-Za-z\d#$@!%&*?]{8,30}$/
